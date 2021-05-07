7ec8cf92a154062416310aee811e2977ce3922f2
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-07 11:18:34 -0300

    Create 7.php

---
+++ 7.php
@@ -0,0 +1,14 @@
+<?php // Settings
+$title="7ort"; // Obligatory!!! Put the name of your blog here
+$description=""; // A description
+$website="https://app.7ort.rf.gd"; // Include protocol (ex. https://example.com)
+$custom_css=""; // Put the url of your custom css
+$utterances=''; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
+// Posts
+$posts=[
+    [
+        "9/4/2021",
+        "Lol",
+        "First post\nNice"
+    ]
+]; ?>
