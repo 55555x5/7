abdd0506d5f33642f1ee5bdd8a1f66407200af93
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-07 23:26:00 -0300

    Update 7.php

--- 7.php
+++ 7.php
@@ -1,9 +1,9 @@
 <?php // Settings
 $title="7ort"; // Obligatory!!! Put the name of your blog here
-$description=""; // A description
+$description="Actualizaciones y más (o menos)"; // A description
 $website="https://app.7ort.rf.gd"; // Include protocol (ex. https://example.com)
 $custom_css="https://raw.githubusercontent.com/7ort/7/main/s.css"; // Put the url of your custom css
-$utterances='<script src="https://utteranc.es/client.js" repo="7ort/7" issue-term="url" theme="github-light" crossorigin="anonymous" async> </script>'; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
+$utterances='<script src="https://utteranc.es/client.js" repo="7ort/7" issue-term="url" theme="github-light" crossorigin="anonymous" async></script>'; // Get it on https://utteranc.es. Please use the "Issue title contains page URL"
 // Posts
 $posts=[
     [
